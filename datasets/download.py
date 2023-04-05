9a1490fe5761ff71832fd9f3beb616ff1c1088f7
Srikanth Srinivas <srikanth.srinivas@mail.utoronto.ca>
2023-04-05 16:35:01 -0700

    fixed download and some scripts

--- datasets/download.py
+++ datasets/download.py
@@ -1,3 +1,4 @@
+import argparse
 import pandas as pd
 
 from dotenv import load_dotenv
@@ -7,10 +8,16 @@ import os
 from huggingface_hub import hf_hub_download
 from sklearn.model_selection import train_test_split
 
+# Set up the command line arguments
+parser = argparse.ArgumentParser(description="Process dataset with max_train and max_test")
+parser.add_argument("--max_train", type=int, help="maximum number of train items")
+parser.add_argument("--max_test", type=int, help="maximum number of test items")
+args = parser.parse_args()
+
 # Replace these values with the appropriate information
-dataset_name = "swype/instruct-66k"
+dataset_name = "swype/instruct"
 HF_TOKEN = os.environ['HF_TOKEN']
-file_path = "diverse.jsonl"  # Replace with the path of the file in the repo
+file_path = "instruct.jsonl"  # Replace with the path of the file in the repo
 destination_dir = "datasets/data"  # Replace with the path to the local directory where you want to save the file
 
 file_spot = hf_hub_download(
@@ -21,19 +28,28 @@ file_spot = hf_hub_download(
     cache_dir=destination_dir,
 )
 
-# Read the data
-main = pd.read_json(file_spot, lines=True)
-secondary = pd.read_json("/root/documents/deepshard/datasets/data.jsonl", lines=True)
-secondary.rename(columns={'response': 'completion'}, inplace=True)
-secondary.drop('source', axis=1, inplace=True)
-merged_df = pd.merge(main, secondary, how='outer')
+def process_data(max_train=None, max_test=None):
+    # Read the data
+    main = pd.read_json(file_spot, lines=True)
 
-# Split the data into train and test sets
-train, test = train_test_split(merged_df, test_size=0.2, random_state=42)
+    # Split the data into train and test sets
+    train, test = train_test_split(main, test_size=0.2, random_state=42)
 
-train = train[:102_400]
-test = test[:1024]
+    # Set default values for max_train and max_test if not provided
+    max_train = max_train or len(train)
+    max_test = max_test or len(test)
 
-# Save the train and test sets as JSONL files
-train.to_json("datasets/data/train.jsonl", orient="records", lines=True)
-test.to_json("datasets/data/test.jsonl", orient="records", lines=True)
+    # Ensure max_train and max_test are not greater than the respective split sizes
+    max_train = min(max_train, len(train))
+    max_test = min(max_test, len(test))
+
+    # Slice the data using max_train and max_test
+    train = train[:max_train]
+    test = test[:max_test]
+
+    # Save the train and test sets as JSONL files
+    train.to_json("datasets/data/train.jsonl", orient="records", lines=True)
+    test.to_json("datasets/data/test.jsonl", orient="records", lines=True)
+
+# Call the function with the command line arguments
+process_data(args.max_train, args.max_test)
